1cb4d8b2a6a87fe94e33021cd84d3d73824046bf
Brad <Brad@BRADSPC>
2020-05-06 15:46:44 -0600

    Added constructors

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+  def __init__(self, name: str, current_room, items: List[str] = []):
+    self.name = name
+    self.current_room = current_room
+    self.items = items
+  
+  def __str__(self) -> str:
+    return "{self.name} is in the {self.current_room.name} \n{self.current_room.description}"
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name: str, description: str, items: List[str]):
+    self.name = name
+    self.description = description
+    self.items = items
+
+  def show_items(self):
+    return F"{self.name} has {len(self.items)} items in it."
+
+  
